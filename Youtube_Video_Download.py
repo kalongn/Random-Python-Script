2e12b3fff03cbde295a239fc67ca8aac008d8b21
Ka Long Ngai <kls8162@gmail.com>
2022-11-13 12:21:18 -0500

    1.1 YT Download Update

--- Youtube_Video_Download.py
+++ Youtube_Video_Download.py
@@ -2,7 +2,7 @@ import pytube
 
 def main():
     url = input("Enter youtube video url: ")
-    path = "" #insert your filePath (Absolute Path) Here.
+    path = input("Enter youtube where the video should go to (Absoltue path): ")
     pytube.YouTube(url).streams.get_highest_resolution().download(path)
 
 if __name__ == "__main__":
